eb086aec192594ec97b7fdc6534e2c2c54997c1f
Marcin Michalski <cyfra0@gmail.com>
2020-04-18 17:08:49 +0200

    added evaluation run

--- multi_player_example.py
+++ multi_player_example.py
@@ -1,6 +1,6 @@
 import os
 
-from kaggle_environments import make, register
+from kaggle_environments import make, register, evaluate
 from kaggle_environments.envs.football import football
 
 env = make("football", debug=True, configuration={"scenario_name": "11_vs_11_stochastic", "team_1": 1, "team_2": 0, "episodeSteps": 100, "render": False, "save_video": True})
@@ -13,4 +13,10 @@ football.cleanup(env)
 print("Logs stored in /tmp/football/%s" % env.id)
 
 
-
+configuration = {"scenario_name": "11_vs_11_stochastic", "team_1": 1, "team_2": 0, "episodeSteps": 100, "render": False, "save_video": True}
+agents = ["run_right", "run_left"]
+rewards = evaluate("football", agents, configuration, steps=[], num_episodes=10)
+## Broken: evaluate looks only on rewards from the last step.
+# (or should we finish after a scored goal??)
+print(rewards)
+football.cleanup_all()
