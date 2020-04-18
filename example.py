637ac2a34d326e17b0081322a5eded5dca78163b
Marcin Michalski <cyfra0@gmail.com>
2020-04-18 15:05:28 +0200

    single simple experiment

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
-from kaggle_environments import make, register
+import os
 
-env = make("football", debug=True, configuration={"scenario_name": "11_vs_11_stochastic", "team_1": 1, "team_2": 1})
+from kaggle_environments import make, register
+from kaggle_environments.envs.football import football
+
+env = make("football", debug=True, configuration={"scenario_name": "11_vs_11_stochastic", "team_1": 1, "team_2": 1, "episodeSteps": 30, "render": True, "save_video": True})
 print(env.name, env.version)
 print("Default Agents: ", *env.agents)
 
 env.run(["run_right", "run_left"])
-# env.render(mode="ipython", width=960, height=720)
+football.cleanup(env)
+print("Logs stored in /tmp/football/%s" % env.id)
