9f878f5c4745f7bd621cc24da1d401800f451aba
Marcin Michalski <cyfra0@gmail.com>
2020-04-19 20:42:16 +0200

    added tf example

---
+++ example_with_tf.py
@@ -0,0 +1,50 @@
+import os
+import tensorflow as tf
+from kaggle_environments import make, register
+from kaggle_environments.envs.football import football
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+import numpy as np
+
+
+## Example with a super-simple keras model.
+
+class DummyKerasModel(object):
+  def __init__(self):
+    model = Sequential()
+    # very simple 1 layer network.
+    model.add(Flatten(input_shape=(72, 96, 4)))
+    model.add(Dense(19, activation='softmax'))
+    model.build()
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+    model.summary()
+    self._model = model
+
+  def train(self, data, labels):
+    self._model.fit(data, labels, epochs=1, batch_size=1)
+
+  def run_agent(self, obs, config, reward, info):
+    print('About to start the agent')
+    minimap = np.reshape(np.array(obs['minimap']), (72, 96, 4))
+    # Add a batch dimension
+    minimap = np.expand_dims(minimap, axis=0)
+    print("Calling the model")
+    action = np.argmax(self._model(minimap))
+    print("Done")
+    # you have to cast it back to int (from numpy.int64)
+    return [int(action)]
+
+
+model = DummyKerasModel()
+# some dummy training.
+model.train(np.zeros((10, 72, 96, 4)), np.ones((10,)))
+
+env = make("football", debug=True, configuration={"scenario_name": "11_vs_11_stochastic", "team_1": 1, "team_2": 1, "episodeSteps": 30, "render": False, "save_video": True})
+print(env.name, env.version)
+print("Default Agents: ", *env.agents)
+
+env.run([model.run_agent, "run_left"])
+football.cleanup(env)
+print("Logs stored in /tmp/football/%s" % env.id)
